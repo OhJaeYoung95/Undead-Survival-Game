ddfc69becddc07e699dec3e16b5e3acad6e46cfa
OhJaeYoung <ostyoung@naver.com>
2023-03-23 15:52:29 +0900

    Player Move

--- Assets/Undead Survivor/Codes/Player.cs
+++ Assets/Undead Survivor/Codes/Player.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    Vector2 inputVec;
+    public Vector2 inputVec;
+    public float speed;
+
+    Rigidbody2D rigid;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        rigid = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        inputVec.x = Input.GetAxis("Horizontal");
-        inputVec.y = Input.GetAxis("Vertical");
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
     }
 }
