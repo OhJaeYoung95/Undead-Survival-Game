f4b82f25c4e94759159847472e655832d0741ebd
OhJaeYoung <ostyoung@naver.com>
2023-03-25 00:44:22 +0900

    Create 2D Cell Animation

--- Assets/Undead Survivor/Codes/Player.cs
+++ Assets/Undead Survivor/Codes/Player.cs
@@ -9,19 +9,23 @@ public class Player : MonoBehaviour
     public float speed;
 
     Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
 
     // Start is called before the first frame update
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    //void Update()
-    //{
-    //    inputVec.x = Input.GetAxisRaw("Horizontal");
-    //    inputVec.y = Input.GetAxisRaw("Vertical");
-    //}
+    void Update()
+    {
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.y = Input.GetAxisRaw("Vertical");
+    }
 
     void FixedUpdate()
     {
@@ -29,8 +33,18 @@ public class Player : MonoBehaviour
         rigid.MovePosition(rigid.position + nextVec);
     }
 
-    void OnMove(InputValue value)
+    void LateUpdate()
     {
-        inputVec = value.Get<Vector2>();
+        anim.SetFloat("Speed", inputVec.magnitude);
+
+        if(inputVec.x != 0)
+        {
+            spriter.flipX = inputVec.x < 0;
+        }
     }
+
+    //void OnMove(InputValue value)
+    //{
+    //    inputVec = value.Get<Vector2>();
+    //}
 }
