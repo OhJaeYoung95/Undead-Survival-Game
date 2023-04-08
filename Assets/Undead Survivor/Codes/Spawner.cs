44be2751167d9ecefda6aec3b826257e6412b676
OhJaeYoung <ostyoung@naver.com>
2023-04-08 13:52:28 +0900

    Spawn Level

--- Assets/Undead Survivor/Codes/Spawner.cs
+++ Assets/Undead Survivor/Codes/Spawner.cs
@@ -17,10 +17,10 @@ public class Spawner : MonoBehaviour
     void Update()
     {
         timer += Time.deltaTime;
-        level = Mathf.FloorToInt(GameManager.instance.gameTime / 10f);
+        level = Mathf.Min(Mathf.FloorToInt(GameManager.instance.gameTime / 10f), spawnData.Length - 1);
         //level = (int)(GameManager.instance.gameTime / 10f);
 
-        if(timer > (level == 0 ? 0.5f : 0.2f))
+        if(timer > spawnData[level].spawnTime)
         {
             timer = 0f;
             Spawn();
@@ -29,15 +29,17 @@ public class Spawner : MonoBehaviour
 
     void Spawn()
     {
-        GameObject enemy = GameManager.instance.pool.Get(level);
+        GameObject enemy = GameManager.instance.pool.Get(0);
         enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position;
+        enemy.GetComponent<Enemy>().Init(spawnData[level]);
     }
 }
 
+[System.Serializable]
 public class SpawnData
 {
-    public int spriteType;
     public float spawnTime;
+    public int spriteType;
     public int health;
     public float speed;
 }
