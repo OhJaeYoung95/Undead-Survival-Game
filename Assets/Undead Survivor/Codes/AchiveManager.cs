b4cc50632cd07815ca64fd25a8bc3976f5a4d6c1
OhJaeYoung <ostyoung@naver.com>
2023-05-02 23:22:27 +0900

    PlayrePrefs q

---
+++ Assets/Undead Survivor/Codes/AchiveManager.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AchiveManager : MonoBehaviour
+{
+    public GameObject[] lockCharacter;
+    public GameObject[] unlockCharacter;
+
+    enum Achive { UnlockPotato, UnlockBean }
+    Achive[] achives;
+
+    void Awake()
+    {
+        achives = (Achive[])Enum.GetValues(typeof(Achive));
+
+        if (!PlayerPrefs.HasKey("MyData"))
+            Init();
+    }
+
+    void Init()
+    {
+        PlayerPrefs.SetInt("MyData", 1);
+
+        foreach(Achive achive in achives)
+        {
+            PlayerPrefs.SetInt(achive.ToString(), 0);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
