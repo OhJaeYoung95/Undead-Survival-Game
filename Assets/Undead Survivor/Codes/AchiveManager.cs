47db3c2e01672dac8baec9122c460005023fc5b0
OhJaeYoung <ostyoung@naver.com>
2023-05-05 21:38:35 +0900

    Create Lock & UnLock System

--- Assets/Undead Survivor/Codes/AchiveManager.cs
+++ Assets/Undead Survivor/Codes/AchiveManager.cs
@@ -7,14 +7,16 @@ public class AchiveManager : MonoBehaviour
 {
     public GameObject[] lockCharacter;
     public GameObject[] unlockCharacter;
+    public GameObject uiNotice;
 
     enum Achive { UnlockPotato, UnlockBean }
     Achive[] achives;
+    WaitForSecondsRealtime wait;  
 
     void Awake()
     {
         achives = (Achive[])Enum.GetValues(typeof(Achive));
-
+        wait = new WaitForSecondsRealtime(5);
         if (!PlayerPrefs.HasKey("MyData"))
             Init();
     }
@@ -47,8 +49,48 @@ public class AchiveManager : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        foreach(Achive achive in achives)
+        {
+            CheckAchive(achive);
+        }
+    }
+
+    void CheckAchive(Achive achive)
+    {
+        bool isAchive = false;
+
+        switch(achive)
+        {
+            case Achive.UnlockPotato:
+                isAchive = GameManager.instance.kill >= 10;
+                break;
+            case Achive.UnlockBean:
+                isAchive = GameManager.instance.gameTime == GameManager.instance.maxGameTime;
+                break;
+        }
+
+        if(isAchive && PlayerPrefs.GetInt(achive.ToString()) == 0)
+        {
+            PlayerPrefs.SetInt(achive.ToString(), 1);
+
+            for(int index = 0; index < uiNotice.transform.childCount; index++)
+            {
+                bool isActive = index == (int)achive;
+                uiNotice.transform.GetChild(index).gameObject.SetActive(isActive);
+            }
+
+            StartCoroutine(NoticeRoutine());
+        }
+    }
+
+    IEnumerator NoticeRoutine()
+    {
+        uiNotice.SetActive(true);
+
+        yield return wait;
+
+        uiNotice.SetActive(false);
     }
 }
