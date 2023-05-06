1743de11ef0f112c2d715c2ff0f9d2dc16e4f640
OhJaeYoung <ostyoung@naver.com>
2023-05-07 00:20:05 +0900

    Audio

---
+++ Assets/Undead Survivor/Codes/AudioManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    [Header("#BGM")]
+    public AudioClip bgmClip;
+    public float bgmVolume;
+    AudioSource bgmPlayer;
+
+    [Header("#SFX")]
+    public AudioClip[] spxClips;
+    public float sfxVolume;
+    public int channels;
+    AudioSource[] sfxPlayers;
+    int channelIndex;
+
+    void Awake()
+    {
+        instance = this;
+        Init();
+    }
+
+    void Init()
+    {
+        // 배경음 플레이어 초기화
+        GameObject bgmObject = new GameObject("BgmPlayer");
+        bgmObject.transform.parent = transform;
+        bgmPlayer = bgmObject.AddComponent<AudioSource>();
+        bgmPlayer.playOnAwake = false;
+        bgmPlayer.loop = true;
+        bgmPlayer.volume = bgmVolume;
+        bgmPlayer.clip = bgmClip;
+
+        // 효과음 플레이어 초기화
+    }
+}
